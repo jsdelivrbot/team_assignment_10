cebb7c46c7bde161ed4504463af36cf48fb9645a
Osvaldo <osvaldofabricio11@hotmail.com>
2018-06-28 10:14:31 -0600

    changes

--- index.js
+++ index.js
@@ -44,3 +44,25 @@ client.query('SELECT NOW()', (err, res) => {
 })
 
 
+// Another query 
+const query = {
+  // give the query a unique name
+  name: 'fetch-user',
+  text: 'SELECT * FROM user WHERE id = $1',
+  values: [1]
+}
+
+// callback
+client.query(query, (err, res) => {
+  if (err) {
+    console.log(err.stack)
+  } else {
+    console.log(res.rows[0])
+  }
+})
+
+// promise
+client.query(query)
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e.stack))
+
