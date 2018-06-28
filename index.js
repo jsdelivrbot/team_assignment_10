0e80d67a3e01b9e085d67ee26ac64e35e8d0895c
Osvaldo <osvaldofabricio11@hotmail.com>
2018-06-28 09:20:23 -0600

    changes

--- index.js
+++ index.js
@@ -20,21 +20,3 @@ express()
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  const pool = new Pool({
-  connectionString: connectionString,
-})
-
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-
-const client = new Client({
-  connectionString: connectionString,
-})
-client.connect()
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
