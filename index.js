115df7b573beec12cfd183c466e9c59817b35d45
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-06-28 19:54:41 -0500

    changes

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ client.query(query, (err, res) => {
   if (err) {
     console.log(err.stack)
   } else {
-    console.log(res.rows[0]['id']),
+    console.log(res.rows[0]['firstName']),
     console.log('THE OTHER ROWWWWWWWWWWWWWWW***************************')
   }
 })
