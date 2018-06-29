c9066155ed2979899e27550e5eec0eab70135e0c
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-06-28 22:04:38 -0500

    changes

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ function connection(req,respond){
 	    console.log('THIS IS PERSON ROWWWWWWWWWWWWWWW***************************'),
 	    console.log(id_Person)
 	    
-	    var firstname = res.row[0].firstname;
-	    var lastname = res.row[0].lastname;
-	    var datebirth = res.row[0].datebirth;
+	    var firstname = res.rows[0].firstname;
+	    var lastname = res.rows[0].lastname;
+	    var datebirth = res.rows[0].datebirth;
 
 	    params = {firstname : firstname, lastname:lastname, datebirth:datebirth};
 	    respond.render('pages/result', params)
