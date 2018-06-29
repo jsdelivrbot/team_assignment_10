858e0fa5c9f79459a50cafef89eda9c5df989c2f
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-06-28 21:43:42 -0500

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/getPerson', function(req, res){
+  .get('/', function(req, res){
 
 	res.render('pages/index');
 
